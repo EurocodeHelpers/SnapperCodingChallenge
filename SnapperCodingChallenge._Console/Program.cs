058fb4f552289df0273ead8a27898e05223e4f11
Peter Cox <support@eurocodehelpers.com>
2019-10-26 23:08:21 +0100

    Modified program.cs

--- SnapperCodingChallenge._Console/Program.cs
+++ SnapperCodingChallenge._Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using SnapperCodingChallenge._Console.Procedural;
 
@@ -9,7 +10,7 @@ namespace SnapperCodingChallenge._Console
         static void Main(string[] args)
         {
             //1. Convert the raw "Snapper" data into a 2d array of characters            
-            char[,] array = ProceduralHelpers.ConvertTxtFileInto2DArray(@"datafiles/perimeterTest.txt");
+            char[,] array = ProceduralHelpers.ConvertTxtFileInto2DArray(@"Test Files/perimeterTest.txt");
             array.Print2DCharacterArrayToConsole();
 
             Console.WriteLine();
@@ -19,9 +20,26 @@ namespace SnapperCodingChallenge._Console
 
             Console.WriteLine();
 
-            var coords = ProceduralHelpers.CalculateCoordinatesInsidePerimeterOfObject(trimmedArray, '0');
+            var coords = ProceduralHelpers.CalculateCoordinatesInsidePerimeterOfObject(trimmedArray, ' ');
             ProceduralHelpers.PrintListOfTuplesToConsole(coords);
 
+            PrintTupleDataToConsole(coords);
         }
+
+        //Cheat method to get static list of tuples...
+
+        public static void PrintTupleDataToConsole(List<Tuple<int,int>> tuples)
+        {
+            Console.WriteLine("var coords = new List<Tuple<int,int>>(){");
+
+            foreach (Tuple<int,int> tuple in tuples)
+            {
+                Console.WriteLine($"new Tuple<int,int>({tuple.Item1},{tuple.Item2}),");
+            }
+                Console.WriteLine("};");
+        }
+
+
+
     }
 }
