7088a0e3bd4cb740f825844f4f5e276c3982822c
Peter Cox <support@eurocodehelpers.com>
2019-10-26 22:49:26 +0100

    Modified program.cs

--- SnapperCodingChallenge._Console/Program.cs
+++ SnapperCodingChallenge._Console/Program.cs
@@ -9,13 +9,19 @@ namespace SnapperCodingChallenge._Console
         static void Main(string[] args)
         {
             //1. Convert the raw "Snapper" data into a 2d array of characters            
-            char[,] array = ProceduralHelpers.ConvertTxtFileInto2DArray(@"C:\Users\pcox\Desktop\git\SnapperCodingChallenge\test.txt");
+            char[,] array = ProceduralHelpers.ConvertTxtFileInto2DArray(@"datafiles/perimeterTest.txt");
             array.Print2DCharacterArrayToConsole();
 
             Console.WriteLine();
 
-            var trimmedArray = ProceduralHelpers.TrimArray(array, 'X');
+            var trimmedArray = ProceduralHelpers.TrimArray(array, '0');
             trimmedArray.Print2DCharacterArrayToConsole();
+
+            Console.WriteLine();
+
+            var coords = ProceduralHelpers.CalculateCoordinatesInsidePerimeterOfObject(trimmedArray, '0');
+            ProceduralHelpers.PrintListOfTuplesToConsole(coords);
+
         }
     }
 }
