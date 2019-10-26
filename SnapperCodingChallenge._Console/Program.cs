25c4d38d945247e53dab7dfc05d362fe4bbbcf38
Peter Cox <support@eurocodehelpers.com>
2019-10-26 17:19:13 +0100

    Added a .NET Core Console Application to test a demo copy of the application.

---
+++ SnapperCodingChallenge._Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using SnapperCodingChallenge._Console.Procedural;
+
+namespace SnapperCodingChallenge._Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Convert the raw "Snapper" data into a 2d array of characters            
+            char[,] array = ProceduralHelpers.ConvertTxtFileInto2DArray(@"C:\Users\pcox\Desktop\git\SnapperCodingChallenge\Starship.blf");
+
+            //2. Print array to console
+            array.Print2DArrayToConsole();
+        }
+    }
+}
