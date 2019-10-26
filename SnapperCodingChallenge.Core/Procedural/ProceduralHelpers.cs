c972b444b346de016352aff0d3154b0dfd9e99de
Peter Cox <support@eurocodehelpers.com>
2019-10-26 21:36:14 +0100

    Methods for converting txt file to 2d array, as well as trimming off empty rows and columns that are not within the shape i.e. compressing the array

--- SnapperCodingChallenge.Core/Procedural/ProceduralHelpers.cs
+++ SnapperCodingChallenge.Core/Procedural/ProceduralHelpers.cs
@@ -1,11 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace SnapperCodingChallenge._Console.Procedural
 {
-    /// <summary>
-    /// A class used to define methods 
-    /// </summary>
     public static class ProceduralHelpers
     {
         /// <summary>
@@ -15,28 +14,27 @@ namespace SnapperCodingChallenge._Console.Procedural
         /// <returns></returns>
         public static char[,] ConvertTxtFileInto2DArray(string txtFile)
         {
-            //TODO Remove any containing white space only from strings[]
-
             //Open textfile and get an array of strings representing each lines, and calc reqd size of 2d array 
             string[] rows = File.ReadAllLines(txtFile);
-            int xSize = rows[0].Length - 1;
-            int ySize = rows.Length;
+
+            int rowNumber = rows.Length;
+            int colNumber = rows[0].Length;
 
             //For each string, convert to array and set the appropriate rows in the char array 
             //char[row,col] remember arrays are 0 based. 
 
-            char[,] array = new char[xSize, ySize];
+            char[,] array = new char[rowNumber, colNumber];
 
             //For each row in the matrix
-            for (int i = 0; i < ySize; i++)
+            for (int i = 0; i < rowNumber; i++)
             {
                 //Convert the ith row into a character array.                     
                 char[] charArray = rows[i].ToCharArray();
 
                 //Add each element in the char array to the row under consideration. 
-                for (int j = 0; j < xSize; j++)
+                for (int j = 0; j < charArray.Length; j++)
                 {
-                    array[j, i] = charArray[j];
+                    array[i,j] = charArray[j];
                 }
             }
 
@@ -47,20 +45,148 @@ namespace SnapperCodingChallenge._Console.Procedural
         /// Prints a 2D array of characters to the console.
         /// </summary>
         /// <param name="array">The 2D array of characters to be printed to the console.</param>
-        public static void Print2DArrayToConsole(this char[,] array)
+        public static void Print2DCharacterArrayToConsole(this char[,] array)
         {
-            int xSize = array.GetLength(0);
-            int ySize = array.GetLength(1);
+            int numberOfRows = array.GetLength(0);
+            int numberOfColumns = array.GetLength(1);
 
-            for (int i = 0; i < ySize; i++)
+            for (int i = 0; i < numberOfRows; i++)
             {
-                for (int j = 0; j < xSize; j++)
+                for (int j = 0; j < numberOfColumns; j++)
                 {
-                    Console.Write(array[j, i]);
+                    Console.Write(array[i, j]);
                 }
                 Console.WriteLine();
             }
         }
+
+        /// <summary>
+        /// Determines whether all the cells in a given col of a 2d array of characters contain any non-white space or ''
+        /// </summary>
+        /// <param name="array">The array to check. </param>
+        /// <param name="col">The col number (0-based).</param>
+        /// <returns></returns>
+        public static bool CheckIfColumnIsBlank(char[,] array, int col, char ch)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[i, col] != ch)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Determines whether all the cells in a given row of a 2d array of characters contain any non-white space or ''
+        /// </summary>
+        /// <param name="array">The array to check. </param>
+        /// <param name="row">The row number (0-based).</param>
+        /// <returns></returns>
+        public static bool CheckIfRowIsBlank(char[,] array, int row, char ch)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                if (array[row, i] != ch)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Removes any blank rows and columns before/after any non blank ones.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static char[,] TrimArray(char[,] array, char ch)
+        {
+            //Get max and min column numbers where not blank.
+            List<int> nonBlankRows = new List<int>();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (CheckIfRowIsBlank(array, i, ch) == false)
+                {
+                    nonBlankRows.Add(i);
+                }
+            }
+
+            int minNonBlankRow = nonBlankRows.Min();
+            int maxNonBlankRow = nonBlankRows.Max();
+
+            //Get max and min row numbers where not blank.
+            List<int> nonBlankCols = new List<int>();
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (CheckIfColumnIsBlank(array, j, ch) == false)
+                {
+                    nonBlankCols.Add(j);
+                }
+            }
+
+            int minNonBlankCol = nonBlankCols.Min();
+            int maxNonBlankCol = nonBlankCols.Max();
+
+            //Copy array between above constraints over to new array to be replaced. 
+            int numberOfRowsReqd = maxNonBlankRow - minNonBlankRow + 1;
+            int numberOfColsReqd = maxNonBlankCol - minNonBlankCol + 1;
+
+            char[,] trimmedArray = new char[numberOfRowsReqd, numberOfColsReqd];
+
+            //For each row in the matrix
+            for (int i = 0; i < numberOfRowsReqd; i++)
+            {
+                for (int j = 0; j < numberOfColsReqd; j++)
+                {
+                    trimmedArray[i,j] = array[i + minNonBlankRow, j + minNonBlankCol];
+                }
+            }
+
+            return trimmedArray;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
