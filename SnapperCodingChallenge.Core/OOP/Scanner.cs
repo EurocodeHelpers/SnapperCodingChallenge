55dfb3344a7a1cf6e7ecf46430e188fbff0cc525
Peter Cox <support@eurocodehelpers.com>
2019-10-29 01:19:35 +0000

    Added method to work out coordinates. Worked on OOP structure.

--- SnapperCodingChallenge.Core/OOP/Scanner.cs
+++ SnapperCodingChallenge.Core/OOP/Scanner.cs
@@ -15,7 +15,16 @@ namespace SnapperCodingChallenge.Core
             //TODO Handle instances where no targets have been specified. 
             Targets.AddRange(from Target t in targets select t);
         }
-        
+
+        public Scanner(Target map, double minimumAccuracy, List<Target> targets)
+        {
+            this.Map = map;
+            this.MinimumAccuracy = minimumAccuracy;
+
+            //TODO Handle instances where no targets have been specified. 
+            Targets.AddRange(from Target t in targets select t);
+        }
+
         public Target Map {get; set; }
         public double MinimumAccuracy { get; set; }
         public List<Target> Targets = new List<Target>();
@@ -23,26 +32,10 @@ namespace SnapperCodingChallenge.Core
         //Methods 
         public void ScanSnapperImage()
         {
-            Console.WriteLine($"LOADING SNAPPER");
-            Console.WriteLine();
-
-            Console.WriteLine("MAP DATA");
-            MultiDimensionalArrayHelpers.Print2DCharacterArrayToConsole(Map.GridRepresentation);
-
-            Console.WriteLine($"TARGETS");
-            foreach (Target t in Targets)
-            {
-                t.PrintTargetInformation();
-            }
-
-            Console.WriteLine($"SCANNING");
             foreach (Target t in Targets)
             {
 
             }
-
-
-
         }
     }
 }
